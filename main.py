302fdd61b1b1b9a7f19b1ab445101cef92a08577
Egor Borisovsky <mr-warete@mail.ru>
2022-03-24 22:54:01 +0300

    Вывод времени выполнения всех моделей вконце

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import os
 datasets = [
     {
         "name": "healthy",
-        "file": "data_fit_with_d_rl_0.csv"
+        "file": "data_fit_with_d_rl_0.csv",
+        "time": 0
     },
     {
         "name": "sick",
-        "file": "data_fit_with_d_rl_1.csv"
+        "file": "data_fit_with_d_rl_1.csv",
+        "time": 0
     },
 ]
 
-for dataset in datasets:
+time_results = []
+
+for k in range(len(datasets)):
+    dataset = datasets[k]
     train_dataset, test_dataset = get_datasets_from_file(os.path.join('data', dataset['file']))
 
     model = RtmModel(dataset['name'])
@@ -22,7 +27,9 @@ for dataset in datasets:
     start_t = time.time()
     model.fit(train_dataset, validation_data=test_dataset, epochs=1000)
     end_t = time.time()
-    print('Finish time: ', end_t - start_t)
+    datasets[k]['time'] = end_t - start_t
     model.save(os.path.join('model_checkpoints', dataset['name']))
 
+for dataset in datasets:
+    print('Finish time', dataset['name'], ':', end_t - start_t)
 #todo: load model
